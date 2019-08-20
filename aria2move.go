2b464cf100c8a293ce8afa3c4014c4a69d9ed049
godcong <jumbycc@163.com>
2019-08-21 03:33:09 +0800

    Create aria2move.go

---
+++ aria2move.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	from := flag.String("from", "./", "from path")
+	to := flag.String("to", "", "to path")
+	flag.Parse()
+
+	sFrom, e := filepath.Abs(*from)
+	if e != nil {
+		panic(e)
+	}
+	files := getFiles(sFrom)
+	fmt.Println("file:", files)
+	fmt.Println("from:", *from)
+	fmt.Println("to:", *to)
+	if *to == "" {
+		panic(e)
+	}
+	sTo, e := filepath.Abs(*to)
+	if e != nil {
+		panic(e)
+	}
+	os.Mkdir(sTo, os.ModePerm)
+	for _, file := range files {
+		_, e := os.Open(file + ".aria2")
+		if !os.IsNotExist(e) {
+			fmt.Println("unfinished", file, "continue", e)
+			continue
+		}
+
+		info, e := os.Stat(file)
+		if e != nil {
+			fmt.Println("error:", e)
+		}
+		_, toFile := filepath.Split(file)
+		//last := len(toFile) - 1
+		fmt.Println("toFile:", toFile)
+		if info.IsDir() {
+			toPath := filepath.Join(sTo, toFile)
+			os.Mkdir(toPath, os.ModePerm)
+			moves := getFiles(file)
+			for _, subFile := range moves {
+				_, subFile = filepath.Split(subFile)
+				e = moveFile(file, filepath.Join(toPath, subFile))
+				if e != nil {
+					fmt.Println(e)
+				}
+			}
+		}else{
+			e = moveFile(file, filepath.Join(sTo, toFile))
+			if e != nil {
+				fmt.Println(e)
+			}
+		}
+	}
+
+}
+
+func getFiles(path string) (files []string) {
+	info, e := os.Stat(path)
+	if e != nil {
+		return nil
+	}
+	if info.IsDir() {
+		file, e := os.Open(path)
+		if e != nil {
+			return nil
+		}
+		defer file.Close()
+		names, e := file.Readdirnames(-1)
+		if e != nil {
+			return nil
+		}
+		var fullPath string
+		for _, name := range names {
+			fullPath = filepath.Join(path, name)
+			if filepath.Ext(fullPath) == ".aria2" || filepath.Ext(fullPath) == ".torrent" {
+				fmt.Printf("continue path:%s\n", fullPath)
+				continue
+			}
+			//tmp := getFiles(fullPath)
+			//if tmp != nil {
+			files = append(files, fullPath)
+			//}
+		}
+		//return files
+	}
+	return files
+}
+func moveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("Couldn't open dest file: %s", err)
+	}
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+	// The copy was successful, so now delete the original file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed removing original file: %s", err)
+	}
+	return nil
+}
