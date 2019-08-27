26529b3e003a07b1b2bd9e3b745e7f0df40af39f
godcong <jumbycc@163.com>
2019-08-28 00:42:24 +0800

    update tracker

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	src := flag.String("src", "tracker.txt", "set the file source")
 	target := flag.String("target", "ouput.txt", "set the output filename")
+	aria := flag.Bool("aria",true,"set the bool to open aria format")
 	flag.Parse()
 
 	file, e := os.Open(*src)
@@ -37,13 +38,17 @@ output:
 		}
 	}
 
-	outFile, e := os.OpenFile(*target, os.O_CREATE|os.O_SYNC|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	outFile, e := os.OpenFile(*target, os.O_CREATE|os.O_SYNC|os.O_RDWR|os.O_TRUNC, os.ModePerm)
 	if e != nil {
 		panic(e)
 	}
 	defer outFile.Close()
 	for key := range results {
 		outFile.WriteString(key)
+		if *aria {
+			outFile.WriteString(",")
+			continue
+		}
 		outFile.WriteString("\n")
 	}
 
