4c8c89150def5637a4e1d334e4c0ac6661b7df0a
godcong <jumbycc@163.com>
2019-09-25 15:46:31 +0800

    update shell

--- aria2shell/shell.go
+++ aria2shell/shell.go
@@ -1,5 +1,64 @@
-package aria2shell
+package main
 
-import "github.com/godcong/go-trait"
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
 
-var log = trait.NewZapSugar()
+	"github.com/zyxar/argo/rpc"
+)
+
+func main() {
+	rpcSecre := flag.String("secret", "", "set --rpc-secret for aria2c")
+	rpcURI := flag.String("uri", "http://localhost:6800/jsonrpc", "set rpc address")
+	filepath := flag.String("path", ".", "get torrent files")
+	flag.Parse()
+
+	client, e := rpc.New(context.Background(), *rpcURI, *rpcSecre, 3*time.Second, nil)
+	if e != nil {
+		log.Fatal(e)
+	}
+	files := getFiles(*filepath)
+
+	for _, file := range files {
+		gid, e := client.AddTorrent(file)
+		if e != nil {
+			log.Println(e)
+			continue
+		}
+		log.Println("gid", gid)
+	}
+}
+
+func getFiles(path string) (files []string) {
+	info, e := os.Stat(path)
+	if e != nil {
+		return nil
+	}
+
+	if info.IsDir() {
+		file, e := os.Open(path)
+		if e != nil {
+			return nil
+		}
+		defer file.Close()
+		names, e := file.Readdirnames(-1)
+		if e != nil {
+			return nil
+		}
+		var fullPath string
+		for _, name := range names {
+			fullPath = filepath.Join(path, name)
+			if filepath.Ext(fullPath) != ".torrent" {
+				log.Println("path", fullPath, "skip")
+				continue
+			}
+			files = append(files, fullPath)
+		}
+	}
+
+	return files
+}
--- aria2shell/shell_test.go
+++
@@ -1,7 +0,0 @@
-package aria2shell
-
-import "testing"
-
-func TestNewRPC(t *testing.T) {
-	NewRPC("http://aria2rpc.y11e.com", "5000", "jsonrpc")
-}
