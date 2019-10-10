5e8cbc36791f64d8c3edf72abd2234632eb26fe2
godcong <jumbycc@163.com>
2019-10-10 13:03:53 +0800

    update shell

--- visualmp4/visualmp4.go
+++ visualmp4/visualmp4.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	from := flag.String("from", "video.txt", "set the from file path")
+	output := flag.String("output", "video", "set the file output path")
 	flag.Parse()
 
 	file, e := os.Open(*from)
@@ -24,7 +26,7 @@ ReadEnd:
 		if e == io.EOF {
 			break ReadEnd
 		}
-		_, _ = os.Create(string(line) + ".mp4")
+		_, _ = os.Create(filepath.Join(*output, string(line)) + ".mp4")
 	}
 	fmt.Println("done")
 
